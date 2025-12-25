f708967cefb2fd0e8145f94183e864e72e1242ff
Joseph Godoy <josephgodoy.t@gmail.com>
2025-12-25 17:55:10 -0600

    Fin ejercicio

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(palabra, cantidad) {
+    let NuevaCadena = palabra;
+    if (cantidad < 0){
+        return 'ERROR';
+    } else if (cantidad == 0) {
+        return "";
+    } else {
+        for (let i = 0; i < cantidad - 1; i++){
+            NuevaCadena = NuevaCadena + palabra;
+        }
+        return NuevaCadena;
+    }
 };
 
 // Do not edit below this line
