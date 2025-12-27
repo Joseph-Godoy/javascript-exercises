33f256a78f654cd9de25c11d5a881a9d927fb15b
Joseph Godoy <josephgodoy.t@gmail.com>
2025-12-26 19:27:04 -0600

    Fin ejercicio

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(Array, ...filtro) {
+    let NuevaArray = Array.filter(n => !filtro.includes(n));
+    return NuevaArray;
 };
 
 // Do not edit below this line
