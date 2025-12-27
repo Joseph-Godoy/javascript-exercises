47d79540fce90bacad4455c5d3617373ba1cb0ae
Joseph Godoy <josephgodoy.t@gmail.com>
2025-12-26 22:50:07 -0600

    Olvide darle ctrl+s

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -4,13 +4,6 @@ const sumAll = function(a, b) {
     let max = Math.max(a, b);
     let n = max - min + 1;
     let sum = (n * (max + min)) / 2;
-    /*
-    if (b < a) {
-        for (let i = b; i <= a; i++) sum += i;
-    } else {
-        for (let i = a; i <= b; i++) sum += i;
-    }*/
-
     return sum;
 };
 
