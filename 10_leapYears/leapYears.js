7b0a107714f11580bd63223e793bcd4e1542b702
Joseph Godoy <josephgodoy.t@gmail.com>
2025-12-26 23:58:48 -0600

    Fin ejercicio

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(Año) {
+    if (((Año % 4 == 0) && !(Año % 100 == 0)) || (Año % 400 == 0)) {
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
