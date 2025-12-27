5ca2f783a55fefda25981739792bb9c03d4d9471
Joseph Godoy <josephgodoy.t@gmail.com>
2025-12-27 00:12:42 -0600

    Fin ejercicio

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Medida) {
+  let Celsius = (Medida - 32) * 5/9;
+  return Number(Celsius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Medida) {
+  let Fahrenheit = (Medida * 9/5) + 32;
+  return Number(Fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
