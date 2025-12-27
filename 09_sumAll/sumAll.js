15521e90040b3c3c3910fd3d9e5a785f0a684f56
Joseph Godoy <josephgodoy.t@gmail.com>
2025-12-26 22:48:55 -0600

    Fin ejercicio

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!(Number.isInteger(a) && Number.isInteger(b)) || (a < 0) || (b < 0)) return "ERROR";
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let n = max - min + 1;
+    let sum = (n * (max + min)) / 2;
+    /*
+    if (b < a) {
+        for (let i = b; i <= a; i++) sum += i;
+    } else {
+        for (let i = a; i <= b; i++) sum += i;
+    }*/
 
+    return sum;
 };
 
 // Do not edit below this line
