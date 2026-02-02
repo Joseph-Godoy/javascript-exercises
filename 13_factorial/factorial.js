d8234bc5f6d135aa8cb80a016f6e37ffa495fcc3
Joseph Godoy <josephgodoy.t@gmail.com>
2026-02-01 23:55:02 -0600

    Fin de ejercicio 13 factorial

--- 13_factorial/factorial.js
+++ 13_factorial/factorial.js
@@ -1,5 +1,13 @@
-const factorial = function() {
-
+const factorial = function (a) {
+    let b = 1;
+    if (!isNaN(a) && Number.isInteger(a) && a >= 0) {
+        while (a > 1) {
+            b = a * b
+            a--
+        }
+        return b;
+    }
+    return undefined;
 };
 
 // Do not edit below this line
